4e1bb8136fea75a732b54182af29effc3e0c9d97
Sinan Sarikaya <sinan.sarykaya@gmail.com>
2023-09-03 00:33:45 +0200

    ADD: Domain

--- main/settings.py
+++ main/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['eneshizli.pythonanywhere.com','127.0.0.1:8000', '127.0.0.1']
+ALLOWED_HOSTS = ['rapidkitchendesign.com','127.0.0.1:8000', '127.0.0.1']
 
 
 # Application definition
